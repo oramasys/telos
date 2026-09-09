2fb4d3ba24c78b43e3605829d0ea939ef84f0f1f
cyre <Lawrence.Melgarejo@gmail.com>
2026-09-10 05:58:32 +0800

    feat(telos): export secure dial contracts

--- src/telos/__init__.py
+++ src/telos/__init__.py
@@ -2,6 +2,7 @@
 from .address import assert_address_allowed, is_metadata_address, is_public_address, parse_ip
 from .authorizer import EndpointAuthorizer
 from .contracts import AuthorizedEndpoint, DecisionRecord, EndpointIdentity, EndpointPurpose, EndpointRef, EndpointUseDecision, EndpointUseRequest, TelosPort
+from .dialer import ConnectedPeer, SecureDialConnector, SecureDialRequest, SecureDialResult, SecureDialer
 from .errors import EndpointPolicyError, TelosError
 from .identity import endpoint_from_url
 from .policy import EndpointPolicy, PurposeRule
@@ -9,10 +10,12 @@ from .resolver import resolve_endpoint
 from .transport import TelosResponse, TransportPolicy, authorize_url, request
 
 __all__ = [
-    "AuthorizedEndpoint", "DecisionRecord", "EndpointAuthorizer", "EndpointIdentity",
-    "EndpointPolicy", "EndpointPolicyError", "EndpointPurpose", "EndpointRef",
-    "EndpointUseDecision", "EndpointUseRequest", "PurposeRule", "TelosError",
-    "TelosPort", "TelosResponse", "TransportPolicy", "assert_address_allowed",
-    "authorize_url", "endpoint_from_url", "is_metadata_address", "is_public_address",
-    "parse_ip", "request", "resolve_endpoint",
+    "AuthorizedEndpoint", "ConnectedPeer", "DecisionRecord", "EndpointAuthorizer",
+    "EndpointIdentity", "EndpointPolicy", "EndpointPolicyError", "EndpointPurpose",
+    "EndpointRef", "EndpointUseDecision", "EndpointUseRequest", "PurposeRule",
+    "SecureDialConnector", "SecureDialRequest", "SecureDialResult", "SecureDialer",
+    "TelosError", "TelosPort", "TelosResponse", "TransportPolicy",
+    "assert_address_allowed", "authorize_url", "endpoint_from_url",
+    "is_metadata_address", "is_public_address", "parse_ip", "request",
+    "resolve_endpoint",
 ]
